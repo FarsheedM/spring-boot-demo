8f22caea838382ef79ca2f2055734fb26937a17c
FarsheedMadihiBidgoli <farsheed.madihi-bidgoli-extern@deutschebahn.com>
2021-04-18 19:53:12 +0200

    the doc is updated+new endpoint added

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ package com.example.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
@@ -12,5 +13,11 @@ public class HelloWorldController {
     @GetMapping("greeting")
     public ResponseEntity<String> greeting(){
         return new ResponseEntity<>("hallo Welt",HttpStatus.OK);
+    }
+
+
+    @GetMapping("greeting/{name}")
+    public ResponseEntity<String> greeting(@PathVariable String name){
+        return new ResponseEntity<>("hallo "+ name,HttpStatus.OK);
     }
 }
